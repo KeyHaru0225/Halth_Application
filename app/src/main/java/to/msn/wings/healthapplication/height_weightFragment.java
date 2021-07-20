5c50259a675b5c45b8c7410b87dd63cbfe549d6b
Kawase Rina <lina.private.tamagoyaki@gmail.com>
2021-07-20 21:03:06 +0900

    パスワード画面、初期画面作成途中。

--- app/src/main/java/to/msn/wings/healthapplication/height_weightFragment.java
+++ app/src/main/java/to/msn/wings/healthapplication/height_weightFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -16,10 +18,33 @@ import androidx.navigation.Navigation;
 public class height_weightFragment extends AppCompatActivity {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater(R.layout.height_weight, container, false);
-        view.findViewById(R.id)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.height_weight);
+
+        mHeight = (EditText) findViewById(R.id.height);  // ユーザーが入力するフィールド(身長)
+        mWeight = (EditText) findViewById(R.id.weight); //　ユーザーが入力するフィールド(体重)
+        mText_bmi = (TextView) findViewById(R.id.text_bmi); // BMI値
+        mHeight_weight_button = (Button) findViewById(R.id.height_weight_button);  //　ボタン
+    }
+
+    //
+    private EditText mHeight;
+    private EditText mWeight;
+    private double mText_bmi = (double) mWeight / mHeight / mHeight; //　BMI=体重÷身長÷身長
+
+
+    public void height_weight_button_onClick(View view) {
+        // ボタンが押された時の処理　→　画面遷移
+        String inputText =
+    }
+
+
+
+
+    
+    public void btnSend_onClick(View view) {
+        EditText txtName = findViewwById(R.id)
     }
 
 }
--- app/src/main/java/to/msn/wings/healthapplication/settingFragment.java
+++ app/src/main/java/to/msn/wings/healthapplication/settingFragment.java
@@ -1,4 +1,71 @@
 package to.msn.wings.healthapplication;
 
-public class settingFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import com.google.android.material.chip.Chip;
+
+import java.util.Objects;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+
+public class settingFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+
+        mSetting = (TextView) findViewById(R.id.setting); //一般設定
+        mSetting_button = (Button) findViewById(R.id.setting_button); // < ボタン
+        mLow_fat_percentage = (TextView) findViewById(R.id.low_fat_percentage); //低脂肪率の記録
+        mPass_lock = (TextView) findViewById(R.id.pass_lock); //パスワードロック
+        mPreventing_forgetting_to_enter = (TextView) findViewById(R.id.preventing_forgetting_to_enter); //入力忘れ防止機能
+        mText_preventing_forgetting_to_enter = (TextView) findViewById(R.id.text_preventing_forgetting_to_enter); //入力忘れの説明
+        mSetting_completed = (Button) findViewById(R.id.setting_completed); //設定完了ボタン
+        mSetting_chip1 = (Chip) findViewById(R.id.setting_chip1); //丸ボタン１
+        mSetting_chip2 = (Chip) findViewById(R.id.setting_chip2); //丸ボタン２
+        mSetting_chip3 = (Chip) findViewById(R.id.setting_chip3); //丸ボタン３
+    }
+
+    private TextView mSetting;
+    private Button mSetting_button;
+    private TextView mLow_fat_percentage;
+    private TextView mPass_lock;
+    private TextView mPreventing_forgetting_to_enter;
+    private TextView mText_preventing_forgetting_to_enter;
+    private Button mSetting_completed;
+    private Chip mSetting_chip1;
+    private Chip mSetting_chip2;
+    private Chip mSetting_chip3;
+
+
+    // ボタンが押された時の処理
+    public void setting_button_onClick(View view) {
+
+    }
+
+    public void setting_completed_onClick(View view) {
+
+    }
+
+    public void setting_chip1_onClick(View view) {
+
+    }
+
+    public void setting_chip2_onClick(View view) {
+
+    }
+
+    public void setting_chip3_onClick(View view) {
+
+    }
+
 }
