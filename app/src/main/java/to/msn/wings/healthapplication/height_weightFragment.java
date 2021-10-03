8baeeb02ff14786a56bfaafa964ad3a8ca4ce245
Kawase Rina <lina.private.tamagoyaki@gmail.com>
2021-10-03 22:25:10 +0900

    gradle

--- app/src/main/java/to/msn/wings/healthapplication/height_weightFragment.java
+++ app/src/main/java/to/msn/wings/healthapplication/height_weightFragment.java
@@ -2,54 +2,65 @@ package to.msn.wings.healthapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.Navigation;
+
 
 
 public class height_weightFragment extends AppCompatActivity {
+
+    private EditText mHeight;
+    private EditText mWeight;
+    private TextView mText_bmi;
+    private Button mHeight_weight_button;
+    private BigDecimal bd;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.height_weight);
 
-        mHeight = (EditText) findViewById(R.id.height);  // ユーザーが入力するフィールド(身長)
-        mWeight = (EditText) findViewById(R.id.weight); //　ユーザーが入力するフィールド(体重)
-        mText_bmi = (TextView) findViewById(R.id.text_bmi); // BMI値
-        mHeight_weight_button = (Button) findViewById(R.id.height_weight_button);  //　ボタン
+        mHeight = (EditText) findViewById(R.id.height);                            // ユーザーが入力するフィールド(身長)
+        mWeight = (EditText) findViewById(R.id.weight);                            // ユーザーが入力するフィールド(体重)
+        mText_bmi = (TextView) findViewById(R.id.text_bmi);                        // BMI値
+        mHeight_weight_button = (Button) findViewById(R.id.height_weight_button);  // ボタン
 
-        ViewPager pager = findViewById(R.id.)
+        // 画面遷移
+        mHeight_weight_button.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplication(), settingFragment.class);
+            startActivity(intent);
+        });
     }
 
 
 
-
-    //
-    private EditText mHeight;
-    private EditText mWeight;
-    private double mText_bmi = (double) mWeight / mHeight / mHeight; //　BMI=体重÷身長÷身長
-
-
-    public void height_weight_button_onClick(View view) {
-        // ボタンが押された時の処理　→　画面遷移
-        String inputText =
-    }
+    // 身長・体重欄に値が入力されているか否かの判定、及びBMI値の出力
+    private double mText_bmi = (double)mWeight / ((double)mHeight * (double)mHeight);  // BMI=体重÷身長÷身長
+    // 結果を小数点第1位までに丸める
+    bd = new BigDecimal(mText_bmi);
+    bd = bd.setScale(1, RoundingMode.HALF_UP);
 
 
+    // 数値入力がされているか否かの判定
+    private String  caution = "数値を入力してください";
 
-
-    
-    public void btnSend_onClick(View view) {
-        EditText txtName = findViewwById(R.id)
+    if("".equals(mHeight) || "".equals(mWeight)) {
+        Toast toast = Toast.makeText (
+                this, caution, Toast.LENGTH_LONG);
+        toast.show();
+    } else {
+        mText_bmi.setText(String.valueOf(bd));
     }
 
 }
--- app/src/main/java/to/msn/wings/healthapplication/settingFragment.java
+++ app/src/main/java/to/msn/wings/healthapplication/settingFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
+import android.content.Intent;
 import com.google.android.material.chip.Chip;
 
 import java.util.Objects;
@@ -21,15 +22,23 @@ public class settingFragment extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting);
+        setContentView(R.layout.setting); //TODO 修正
 
         mSetting = (TextView) findViewById(R.id.setting); //一般設定
         mSetting_button = (Button) findViewById(R.id.setting_button); // < ボタン
+        mSetting_button.setOnClickListener(view -> finish()); // 身長体重(初期設定画面)に戻る
+
         mLow_fat_percentage = (TextView) findViewById(R.id.low_fat_percentage); //低脂肪率の記録
         mPass_lock = (TextView) findViewById(R.id.pass_lock); //パスワードロック
         mPreventing_forgetting_to_enter = (TextView) findViewById(R.id.preventing_forgetting_to_enter); //入力忘れ防止機能
         mText_preventing_forgetting_to_enter = (TextView) findViewById(R.id.text_preventing_forgetting_to_enter); //入力忘れの説明
         mSetting_completed = (Button) findViewById(R.id.setting_completed); //設定完了ボタン
+        // helloFragmentへ遷移
+        mSetting_completed.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplication(), helloFragment.class);
+            startActivity(intent);
+        })
+
         mSetting_chip1 = (Chip) findViewById(R.id.setting_chip1); //丸ボタン１
         mSetting_chip2 = (Chip) findViewById(R.id.setting_chip2); //丸ボタン２
         mSetting_chip3 = (Chip) findViewById(R.id.setting_chip3); //丸ボタン３
